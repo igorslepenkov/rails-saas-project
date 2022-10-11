08976341c96fb9da64575a3f4b8bb533876faf60
igorslepenkov <slepenkov.nii@yandex.by>
2022-10-11 15:39:44 +0300

    feat(saas-project): implement functionality of upgrading plan from free to premium with stripe payment

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,4 +12,21 @@ class PagesController < ApplicationController
     @domain = request.host_with_port
     @projects = Project.all
   end
+
+  def edit_plan
+    @organization = current_organization
+  end
+
+  def upgrade_plan
+    @organization = current_organization
+    @organization.update(upgrade_plan_params)
+    @organization.update(plan: 'premium')
+    redirect_to pages_dashboard_path, notice: 'Plan changed successfully'
+  end
+
+  private
+
+  def upgrade_plan_params
+    params.require(:organization).permit(:payment)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,12 +7,20 @@ Rails.application.routes.draw do
     invitations: 'users/invitations',
     sessions: 'users/sessions'
   }
+
   root 'pages#home'
+  
   get 'pages/about', to: 'pages#about'
   get 'pages/plans', to: 'pages#plans'
   get 'pages/dashboard', to: 'pages#dashboard'
+  get 'upgrade_plan', to: 'pages#edit_plan'
+  patch 'upgrade_plan', to: 'pages#upgrade_plan'
+  put 'upgrade_plan', to: 'pages#upgrade_plan'
+
   resources :projects
   resources :artifacts
+
   post 'payments/create-intent', to: 'payments#create_intent'
+
   post 'validations/organization', to: 'validations#organization_valid?'
 end
