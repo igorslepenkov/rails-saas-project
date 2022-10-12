f3f1a937591864cbde49f4ce8f7876ffaffdb7d1
igorslepenkov <slepenkov.nii@yandex.by>
2022-10-12 12:49:31 +0300

    fix(saas-project): add default host to mailer in production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,8 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "rails_sass_project_production"
 
+  config.action_mailer.default_url_options = { host: 'https://igor-saas-project.herokuapp.com/' }
+
   config.action_mailer.perform_deliveries = true
 
   config.action_mailer.raise_delivery_errors = true
